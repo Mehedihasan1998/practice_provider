1523477ea36949cb86123ca3cea31c436b0b30da
Md Mehedi Hasan <mehedi5477@gmail.com>
2024-03-05 16:02:41 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:practice_provider/manage_state.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  ManageState manageState = ManageState();
+
+  void _incrementCounter(ManageState ms) {
+    ms.counter++;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider.value( value: manageState,
+      child: Consumer<ManageState>(
+        builder: (context, ms, __){
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+              title: Text(widget.title),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    'You have pushed the button this many times:',
+                  ),
+                  Text(
+                    '${ms.counter}',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                ],
+              ),
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () => _incrementCounter(ms),
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
